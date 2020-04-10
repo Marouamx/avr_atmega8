3fbe555ecec7874538ef4f97ce47be8c62ea5536
Maroua Bellatreche <62667666+Marouamx@users.noreply.github.com>
2020-04-10 04:56:22 +0100

    Update and rename usart_h.c to usart_h.h

--- usart_h/usart_h.h
+++ usart_h/usart_h.h
@@ -1,4 +1,4 @@
-#ifndef USARTmx_H
+ifndef USARTmx_H
 #define USARTmx_H
 
 #include<avr/io.h>
@@ -8,13 +8,14 @@
 
 typedef struct {
 
-    unsigned _9th_Tdata  : 1 ;
+    unsigned _9th_Tdata  : 1 ; // used for diffrentiating adresses from data  multi processor mode
     unsigned _9th_Rdata  : 1 ;
     unsigned _charSize   : 1 ;
-    unsigned _enable_tx  : 1 ;
-    unsigned _enable_rx  : 1 ;
-    unsigned             : 3 ;
-
+    unsigned _tx  : 1 ;
+    unsigned _rx  : 1 ;
+    unsigned _Globl_intp : 1 ;
+    unsigned _tx_intp    : 1 ;
+    unsigned _rx_intp    : 1 ;
 } UCSR0Bbits_t ;
 
 extern volatile UCSR0Bbits_t enable __asm__ ("0xC1");
@@ -33,12 +34,12 @@ extern volatile UCSR0Cbits_t config __asm__ ("0xC2");
 
 typedef struct {
 
-    unsigned               : 2 ;
+    unsigned               : 2 ; // multiSlave option
     unsigned _Parity_error : 1 ; // R n T parity dont match
     unsigned _OverRun      : 1 ; //R buffer full (2chars) but new start bit is detected
     unsigned _Frame_error  : 1 ; // stop bit not received
-    unsigned _Buffer_empty : 1 ; // ie ready to receive new data
-    unsigned _T_complete   : 1 ; // set when T buffer cleared
+    unsigned _UDRE0 : 1 ; // UDRE0 data register empty interrupt
+    unsigned _T_complete   : 1 ; // set when T buffer cleared- automatically cleared when transmit interrupt is excuted
     unsigned _R_complete   : 1 ; // set for unread data in buffer
 
 
@@ -50,19 +51,19 @@ extern volatile char BD_low_bits __asm__ ("0xC4");
 extern volatile char BD_high_bits __asm__ ("0xC5");
 
 
-void usart_init(long  Baud_rate) {
+void usart_init(unsigned long Baud_rate) {
 
   unsigned int ubrr = F_CPU/16/Baud_rate - 1 ;
   BD_high_bits = (unsigned char) (ubrr>>8) ;
   BD_low_bits = (unsigned char) ubrr;
-  enable._enable_tx = 1 ;
-  enable._enable_rx = 1 ;
+  enable._tx = 1 ;
+  enable._rx = 1 ;
   config._charSize =  3 ; // 8 bits data
 
 }
 
 
-void usart_advanced_init (long  Baud_rate, char _Parity , char _stopBit , char _mode , char _charSize ) {
+void usart_advanced_init (unsigned long Baud_rate, char _Parity , char _stopBit , char _mode , char _charSize ) {
 
   unsigned int ubrr = F_CPU/16/Baud_rate - 1 ;
 
@@ -74,8 +75,8 @@ void usart_advanced_init (long  Baud_rate, char _Parity , char _stopBit , char _
   BD_high_bits = (unsigned char) (ubrr>>8) ;
   BD_low_bits = (unsigned char) ubrr;
 
-  enable._enable_tx = 1 ;
-  enable._enable_rx = 1 ;
+  enable._tx = 1 ;
+  enable._rx = 1 ;
 
 
 }
@@ -83,64 +84,49 @@ void usart_advanced_init (long  Baud_rate, char _Parity , char _stopBit , char _
 void usart_transmit_int(unsigned int data) {
 
 
-  while (!(debug._T_complete == 0)) ; // wait for empty T buffer
+  while (debug._UDRE0 == 0) ; // wait for empty T buffer
 
   if (config._charSize > 3) { // bigger than 8 bits
       enable._9th_Tdata = 0 ;
       if ( data & 0x100) {
           enable._9th_Tdata = 1 ;
-          UDR = data ;
+          UDR0 = data ;
       }
   }
-  else  UDR = data ;
+  else  UDR0 = data ;
 
 }
 
 
 void usart_transmit(unsigned char data) {
 
-  while (!(debug._T_complete == 0)) ; // wait for empty T buffer
-  UDR = data ;
+    while (debug._UDRE0 == 0) ; // wait for empty T buffer
+    UDR0 = data ;
 
 }
 
 
 unsigned char usart_receive () {
 
-  while (!(debug._R_complete == 0)) ; // wait for empty R buffer
+  while (debug._R_complete == 0) ; // wait for empty R buffer
 
-  if (debug._Parity_error = 1 ) return 0;
-  if (debug._Frame_error =  1 ) return-1;
-  if (debug._OverRun =      1 ) return 1;
+  if (debug._Parity_error == 1 || debug._Frame_error == 1 ) return 0;
+  if (debug._OverRun == 1 ) return -1;
 
 
-  return UDR ;
+  return UDR0 ;
 }
 
-void usart_transmit_string (unsigned char* data) {
+void usart_transmit_string (char* data) {
 
-    for (int i =0 ; data[i] != '/n' ; i++) {
+    for (int i =0 ; data[i] != 0 ; i++) {
 
-        while (!(debug._T_complete == 0)) ; // wait for empty T buffer
-        UDR = data[i];
-    }
-}
+        usart_transmit(data[i]);
 
-
-char* usart_receive_string () {
-
-    char buffer[10000] ;
-    int i = 0;
-    do {
-        while (!(debug._R_complete == 0)) ; // wait for empty R buffer
-        buffer[i]= (char) UDR;
-        i++;
-    } while (UDR != '\n');
-
-
-    return buffer;
+        }
 }
 
 
 
 #endif
+
