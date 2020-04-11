696cac32336e399a727f51543259a2e756823502
Maroua Bellatreche <62667666+Marouamx@users.noreply.github.com>
2020-04-11 19:03:01 +0100

    Update ISRinterrupt.c

--- UART/ISRinterrupt.c
+++ UART/ISRinterrupt.c
@@ -1,30 +1,63 @@
+/*****
+
+    Maroumx
+
+compiler : avr-gcc
+
+******/
+
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
+#include "UARTmx.h"
+
 #define USART_BAUDRATE 9600
-#define BUF_SIZE 20
+#define BUF_SIZE 50
 
-int main (void)
-{
-   UCSRB = (1 << RXEN) | (1 << TXEN);   // Turn on the transmission and reception circuitry
-   UCSRC = (1 << URSEL) | (1 << UCSZ0) | (1 << UCSZ1); // Use 8-bit character sizes
 
-   UBRRH = (BAUD_PRESCALE >> 8); // Load upper 8-bits of the baud rate value into the high byte of the UBRR register
-   UBRRL = BAUD_PRESCALE; // Load lower 8-bits of the baud rate value into the low byte of the UBRR register
 
-   UCSRB |= (1 << RCXIE); // Enable the USART Recieve Complete interrupt (USART_RXC)
-   sei(); // Enable the Global Interrupt Enable flag so that interrupts can be processed
+unsigned char buff[BUF_SIZE];
 
-   for (;;) // Loop forever
-   {
-         // Do nothing - echoing is handled by the ISR instead of in the main loop
-   }
+
+//RX Complete interrupt service routine
+ISR(USART_RXC_vect) {
+
+    usart_receive_string(buff);
+    //disable rx transmittion and interrupt
+    enable._rx = 0;
+    enable._rx_intp = 0;
+    //enable empty Buffer interrupt and tx Receive
+    enable._tx = 1;
+    enable._Globl_intp =1;
 }
 
-ISR(USART_RXC_vect)
-{
-   char ReceivedByte;
-   ReceivedByte = UDR; // Fetch the received byte value into the variable "ByteReceived"
-   UDR = ReceivedByte; // Echo back the received byte back to the computer
+//UDR0 Empty interrupt service routine
+ISR(USART_UDRE_vect) {
+
+    usart_transmit_string("well received");
+    // disable empty Buffer interrupt and tx Receive
+    enable._tx = 0;
+    enable._Globl_intp =0;
+    //enable rx and rx interrupt
+    enable._rx =1;
+    enable._rx_intp =1;
+
 }
+
+int main (void) {
+
+   usart_init(USART_BAUDRATE);
+   set_sleep_mode(SLEEP_MODE_IDLE);
+
+   sei(); // Enable the Global Interrupt
+
+   enable._rx_intp = 1 ;
+
+    while (1) {
+
+        sleep_mode();
+
+    }
+}
+
